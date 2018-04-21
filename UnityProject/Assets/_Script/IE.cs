faba564de63314c37fb97294c97232d7a9a6b683
Olivier Léonard <olivierr.leonard@gmail.com>
2018-04-21 12:24:21 +0200

    IE

---
+++ UnityProject/Assets/_Script/IE.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+    public class IE: MonoBehaviour
+    {
+        public SpriteRenderer m_spriteRenderer;
+        public BoxCollider2D m_collider;
+        
+        void OnTriggerEnter2D(Collider2D other) {
+              m_spriteRenderer.color = Color.red;  
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            m_spriteRenderer.color = Color.white;
+        }
+    }
