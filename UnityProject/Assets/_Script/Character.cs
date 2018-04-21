8ab0a726aa9c886ed5f4cd5fa99970ee6612791a
LIM-PC\arnau <amiskovic.work@gmail.com>
2018-04-21 12:21:42 +0200

    character move

--- UnityProject/Assets/_Script/Character.cs
+++ UnityProject/Assets/_Script/Character.cs
@@ -5,6 +5,26 @@ namespace DefaultNamespace
 {
   public class Character : MonoBehaviour
   {
+    [SerializeField]
+    private float _moveForceRatio = 100;
+    
     public PlayerInput _input;
+    
+    private Rigidbody2D _rb;
+
+    void Awake()
+    {
+      _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+      if (_input == null)
+        return;
+
+      var stickVector = new Vector2(_input.H1(), _input.V1());
+      
+      _rb.AddForce(stickVector * _moveForceRatio);
+    }
   }
 }
