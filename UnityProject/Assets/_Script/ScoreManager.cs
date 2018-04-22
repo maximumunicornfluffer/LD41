08e04d020a8fa7b100db5575fbefd621eda3160d
jeromefirlej <tintale@free.fr>
2018-04-22 18:44:12 +0200

    refactor score

--- UnityProject/Assets/_Script/ScoreManager.cs
+++ UnityProject/Assets/_Script/ScoreManager.cs
@@ -6,6 +6,7 @@ namespace DefaultNamespace
 {
     public class ScoreManager : MonoBehaviour
     {
+        private string scorePattern = "score : {0}";
         private int _score;
 
         public Text ScoreText;
@@ -13,13 +14,13 @@ namespace DefaultNamespace
         public void Start()
         {
             _score = 0;
-            ScoreText.text = _score.ToString();
+            ScoreText.text = string.Format(scorePattern, _score.ToString());
         }
 
         public void Add(int points)
         {
             _score += points;
-            ScoreText.text = _score.ToString();
+            ScoreText.text = string.Format(scorePattern, _score.ToString());
         }
         
     }
