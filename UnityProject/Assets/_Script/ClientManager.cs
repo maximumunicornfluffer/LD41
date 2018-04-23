aa8b0ca0728696c4141dff992e8c67422154d172
Olivier Léonard <olivierr.leonard@gmail.com>
2018-04-23 20:38:27 +0200

    client pop tweak + launcher pic

--- UnityProject/Assets/_Script/ClientManager.cs
+++ UnityProject/Assets/_Script/ClientManager.cs
@@ -11,17 +11,19 @@ namespace DefaultNamespace
         [SerializeField] private Client _clientPrefab;
         [SerializeField] private Client _kidClientPrefab;
         [SerializeField] private Client _titineClientPrefab;
+
+        private int m_clientCount;
         private List<Client> _clients;
         private float _lastClientPop;
         private Vector3[] _waitPositionsPossible;
 
-        private float m_clientPopInterval;        
-
+        private float m_clientPopInterval;
+        
 		public MusicManager _musicManager;
 
         private void GenerateWaitPoints()
-        {
-            m_clientPopInterval = 20.0f / PlayersManager.Instance.Players.Count;
+        { 
+            m_clientPopInterval = 30.0f / PlayersManager.Instance.Players.Count;
             
             _waitPositionsPossible = new Vector3[10];
             _waitPositionsPossible[0] = GameManager.Instance.LevelData.ClientPoints[1].localPosition;
@@ -50,6 +52,24 @@ namespace DefaultNamespace
 
         private void AddNewClient()
         {
+            var playersCount = PlayersManager.Instance.Players.Count;
+            switch (playersCount)
+            {
+                case 1: 
+                    m_clientPopInterval = Mathf.Clamp(25.0f - m_clientCount*3f, 15f, 25f) / playersCount;
+                    break;    
+                case 2: 
+                    m_clientPopInterval = Mathf.Clamp(27.5f - m_clientCount*2f, 17.5f, 27.5f) / playersCount;
+                    break;    
+                case 3: 
+                    m_clientPopInterval = Mathf.Clamp(30f - m_clientCount*1.5f, 20f, 30f) / playersCount;
+                    break;    
+                case 4: 
+                    m_clientPopInterval = Mathf.Clamp(32.5f - m_clientCount*1f, 22.5f, 32.5f) / playersCount;
+                    break;    
+            }
+            m_clientCount++;
+            
             var client = Instantiate(DetermineClientType());
 
             for (var cpt = 0; cpt < 10; cpt++)
