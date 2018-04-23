2975337ebaf65d4acade7ad9081249164d1ebb14
Alexis Zbik <yamaneko@Alexiss-Mac-mini.local>
2018-04-23 15:21:04 +0200

    game over , restart after 2s

--- UnityProject/Assets/_Script/GameoverScript.cs
+++ UnityProject/Assets/_Script/GameoverScript.cs
@@ -7,7 +7,8 @@ using States;
 using UnityEngine.SceneManagement;
 using DefaultNamespace;
 
-public class GameoverScript : MonoBehaviour {
+public class GameoverScript : MonoBehaviour
+{
 
 	public Sprite highlightedSprite;
 	public Text scoreText;
@@ -16,41 +17,58 @@ public class GameoverScript : MonoBehaviour {
 	private SpriteRenderer srenderer;
 	private AudioSource audio;
 
+	private float timeToWaitBeforeUpdate = 2.0f;
+	private float waitingTime = 0.0f;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 
-		srenderer = gameObject.GetComponent<SpriteRenderer>();
-		audio = gameObject.GetComponent<AudioSource>();
 
-		scoreText.text = "Your " + GameManager.Instance.GetScoreText();
+		srenderer = gameObject.GetComponent<SpriteRenderer> ();
+		srenderer.enabled = false;
+		audio = gameObject.GetComponent<AudioSource> ();
+
+		scoreText.text = "Your " + GameManager.Instance.GetScoreText ();
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 
-		bool shouldGoToLobby = false;
+		waitingTime = waitingTime + Time.deltaTime;
 
-		var validInputIndexes = PlayerInputUtils.GetValidInputIndexes();
-		foreach (var index in validInputIndexes)
+		if (!srenderer.enabled && waitingTime > timeToWaitBeforeUpdate)
 		{
-			if (PlayerInputUtils.GetButtonDown(PlayerInput._A, index)) {
-				shouldGoToLobby = true;
+			srenderer.enabled = true;
+		}
+			
+		if (srenderer.enabled)
+		{
+			bool shouldGoToLobby = false;
+			var validInputIndexes = PlayerInputUtils.GetValidInputIndexes ();
+			foreach (var index in validInputIndexes)
+			{
+				if (PlayerInputUtils.GetButtonDown (PlayerInput._A, index))
+				{
+					shouldGoToLobby = true;
+				}
+			} 
+
+			if (Input.GetKeyDown (KeyCode.Space) || shouldGoToLobby)
+			{
+				srenderer.sprite = highlightedSprite;
+				audio.Play ();
+
+				Debug.Log ("restart");
+
+				GameManager.Instance.ResetScore ();
+
+				FSM.Instance.GotoState<LobbyState> ();
+				gameObject.AddComponent<PlayersManager> ();
+
+				//Load next scene here
 			}
-		} 
-
-		if (Input.GetKeyDown(KeyCode.Space) || shouldGoToLobby)
-		{
-			srenderer.sprite = highlightedSprite;
-			audio.Play();
-
-			Debug.Log("restart");
-
-			GameManager.Instance.ResetScore();
-
-			FSM.Instance.GotoState<LobbyState>();
-			gameObject.AddComponent<PlayersManager>();
-
-			//Load next scene here
 		}
 	}
 }
